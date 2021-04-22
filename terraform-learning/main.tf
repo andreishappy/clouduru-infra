98996f10adf19fe0f26c806667d156e8a4b21c23
Andrei Petric <andrei.petric@octoenergy.com>
2021-04-22 10:19:22 +0100

    Add one EC2 hello world instance

--- terraform-learning/main.tf
+++ terraform-learning/main.tf
@@ -1,5 +1,41 @@
+# --------
+# Provider
+# --------
+
 provider "aws" {
   region = "us-east-1"
+}
+
+# ---------
+# Variables
+# ---------
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
+# -------
+# Outputs
+# -------
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+}
+
+# ---------
+# Resources
+# ---------
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "example" {
@@ -9,4 +45,12 @@ resource "aws_instance" "example" {
   tags = {
     Name = "terraform-example"
   }
+
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
 }
