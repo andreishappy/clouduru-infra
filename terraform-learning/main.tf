8e6dd43c3362bc8e6883f8d7dc0a49a6d7d58415
Andrei Petric <andrei.petric@octoenergy.com>
2021-04-23 08:33:38 +0100

    Add remote state and locking

--- terraform-learning/main.tf
+++ terraform-learning/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-learning-terraform-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+
+    dynamodb_table = "terraform-learning-locks"
+    encrypt        = true
+  }
+}
+
 # --------
 # Provider
 # --------
---
+++ terraform-state/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-learning-terraform-state"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-learning-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
