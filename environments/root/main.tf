d7ccfb6ac0862d6f9508f12096b05547efd1d852
Andrei Petric <andrei.petric@octoenergy.com>
2021-05-14 17:39:17 +0100

    Root environment with s3 and dynamo db for terraform state

--- environments/root/main.tf
+++ environments/root/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-1"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-learning-terraform-state"
+  bucket = "clouduru-terraform-state"
 
   versioning {
     enabled = true
@@ -15,7 +15,7 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-learning-locks"
+  name         = "clouduru-terraform-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
